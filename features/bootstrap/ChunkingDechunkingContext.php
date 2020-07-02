531e81c253fc9787d0b12b64682f1b662265aef7
Alexandre-T <alexandre.tranchant@gmail.com>
2020-07-02 11:34:42 +0200

    Fixing declaration because of PhpUnit8.

--- features/bootstrap/ChunkingDechunkingContext.php
+++ features/bootstrap/ChunkingDechunkingContext.php
@@ -1,11 +1,8 @@
 <?php
 
-use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
-use PHPUnit_Framework_Assert as Assert;
+use PHPUnit\Framework\Assert;
 
 /**
  * Defines application features from the specific context.
